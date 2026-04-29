be47d8ebba2c7a01e31131dc376de423bc99f635
Arya Patel <abhipatel867@gmail.com>
2026-04-28 21:44:01 -0500

    Initial commit for MLOps project

---
+++ model.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+def train_model():
+    print("Training ML model...")
+    # Model training code would go here
+
+if __name__ == "__main__":
+    print("MLOps Project Initialized")
+    train_model()
